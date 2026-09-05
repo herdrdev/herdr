b24cda0b37c198657fc78c59c334d62c17ef3792
Jonathan Liebig <jonathan.liebig@gmail.com>
2026-09-05 17:24:51 +0200

    fix: run the full applicable test suite on windows

--- src/server/headless/tests/mod.rs
+++ src/server/headless/tests/mod.rs
@@ -18,7 +18,7 @@ fn test_headless_server() -> HeadlessServer {
 fn test_headless_server_with_event_hub(event_hub: api::EventHub) -> HeadlessServer {
     let config = crate::config::Config::default();
     let (_api_tx, api_rx) = tokio::sync::mpsc::unbounded_channel();
-    let app = crate::app::App::new(
+    let mut app = crate::app::App::new(
         &config,
         crate::app::AppPolicy::TEST,
         None,
@@ -26,6 +26,7 @@ fn test_headless_server_with_event_hub(event_hub: api::EventHub) -> HeadlessServ
         event_hub,
     );
 
+    app.state.default_shell = crate::app::exiting_test_command().into();
     let dir = std::env::temp_dir().join(format!(
         "hh-{}-{}",
         std::process::id(),
